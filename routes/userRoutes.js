ff38071cccfeab21437eb943c032170a0ffb11ea
hayleamlak <hayleamlaktesfaye12@gmail.com>
2025-08-17 11:09:41 +0300

    Remove node_modules from Git tracking and use espress to add user datan in mongobdb for first time

---
+++ routes/userRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+// Create a new user
+router.post("/", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Get all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+export default router;
